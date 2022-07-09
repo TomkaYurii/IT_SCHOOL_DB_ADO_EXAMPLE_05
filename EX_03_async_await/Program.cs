e5a08080c89a363d36d83127526220d13792fc07
Tomka Yuriy <tomka.yurii@gmail.com>
2022-07-09 09:56:42 +0300

    final

---
+++ EX_03_async_await/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EX_03_async_await
+{
+    class Account
+    {
+        int sum = 0;
+        public event EventHandler<string>? Added;
+        public void Put(int sum)
+        {
+            this.sum += sum;
+            Added?.Invoke(this, $"На счет поступило {sum} $");
+        }
+    }
+
+
+    internal class Program
+    {
+        static Task Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenuAsync().Result;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task<bool> MainMenuAsync()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) void");
+            Console.WriteLine("2) void - обработка событий");
+            Console.WriteLine("3) Task");
+            Console.WriteLine("4) Task<T>");
+            Console.WriteLine("5) ValueTask<T>");
+            Console.WriteLine("0) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    PrintAsync("Hello World!!!");
+                    PrintAsync("Hello GUYS!!!");
+                    Console.WriteLine("Main End");
+                    await Task.Delay(3000); // ждем завершения задач
+                    Console.ReadKey();
+                    return true;
+
+                case "2":
+                    Account account = new Account();
+                    account.Added += PrintAsync;
+                    account.Put(500);
+                    await Task.Delay(2000); // ждем завершения
+                    Console.ReadKey();
+                    return true;
+
+                case "3":
+                    await HelloAsync("Hello Guys!");
+                    Console.WriteLine("Main Works");
+                    Console.ReadKey();
+                    return true;
+
+                case "4":
+                    var square5 = SquareAsync(5);
+                    var square6 = SquareAsync(6);
+
+                    Console.WriteLine("Остальные действия в методе Main");
+
+                    int n1 = await square5;
+                    int n2 = await square6;
+                    Console.WriteLine($"n1={n1}  n2={n2}"); // n1=25  n2=36
+
+                    Console.ReadKey();
+                    return true;
+
+                case "5":
+                    var result = await AddAsync(4, 5);
+                    Console.WriteLine(result);
+
+                    Console.ReadKey();
+                    return true;
+                case "0":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+        // --- 01 ---
+        async static void PrintAsync(string message)
+        {
+            await Task.Delay(2000);     // имитация продолжительной работы
+            Console.WriteLine(message);
+        }
+        // --- 02 ---
+        async static void PrintAsync(object? obj, string message)
+        {
+            await Task.Delay(1000);     // имитация продолжительной работы
+            Console.WriteLine(message);
+        }
+
+        // --- 03 ---
+        async static Task HelloAsync(string message)
+        {
+            await Task.Delay(1000);     // имитация продолжительной работы
+            Console.WriteLine(message);
+        }
+
+        // --- 04 ---
+        async static Task<int> SquareAsync(int n)
+        {
+            await Task.Delay(0);
+            var result = n * n;
+            Console.WriteLine($"Квадрат числа {n} равен {result}");
+            return result;
+        }
+
+        // --- 05 ---
+        static ValueTask<int> AddAsync(int a, int b)
+        {
+            return new ValueTask<int>(a + b);
+        }
+    }
+}
---
+++ EX_04_async_await/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EX_04_async_await
+{
+    internal class Program
+    {
+        static Task Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenuAsync().Result;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task<bool> MainMenuAsync()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Демонстрация блокировки асинхронности синхронностью");
+            Console.WriteLine("2) Недетерминированный вывод");
+            Console.WriteLine("3) Task.WhenAll");
+            Console.WriteLine("4) Task.WhenAny");
+            Console.WriteLine("5) Получение резульатата 01");
+            Console.WriteLine("6) Получение резульатата 02");
+            Console.WriteLine("0) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    await PrintAsync("Hello C#");
+                    await PrintAsync("Hello World");
+                    await PrintAsync("Hello GUYS!");
+
+                    Console.ReadKey();
+                    return true;
+
+                case "2":
+                    // определяем и запускаем задачи
+                    var task1 = PrintAsync("Hello C#");
+                    var task2 = PrintAsync("Hello World");
+                    var task3 = PrintAsync("Hello Guys!");
+
+                    // ожидаем завершения задач
+                    await task1;
+                    await task2;
+                    await task3;
+
+                    Console.ReadKey();
+                    return true;
+
+                case "3":
+                    // определяем и запускаем задачи
+                    var task01 = PrintAsync("Hello C#");
+                    var task02 = PrintAsync("Hello World");
+                    var task03 = PrintAsync("Hello METANIT.COM");
+
+                    // ожидаем завершения всех задач
+                    await Task.WhenAll(task01, task02, task03);
+
+                    Console.ReadKey();
+                    return true;
+                case "4":
+                    // определяем и запускаем задачи
+                    var task001 = PrintAsync_04("Hello C#");
+                    var task002 = PrintAsync_04("Hello World");
+                    var task003 = PrintAsync_04("Hello METANIT.COM");
+
+                    // ожидаем завершения хотя бы одной задачи
+                    await Task.WhenAny(task001, task002, task003);
+
+                    Console.ReadKey();
+                    return true;
+
+                case "5":
+                    // определяем и запускаем задачи
+                    var taskx = SquareAsync(4);
+                    var tasky = SquareAsync(5);
+                    var taskz = SquareAsync(6);
+
+                    // ожидаем завершения всех задач
+                    int[] results = await Task.WhenAll(taskx, tasky, taskz);
+                    // получаем результаты:
+                    foreach (int result in results)
+                        Console.WriteLine(result);
+
+                    Console.ReadKey();
+                    return true;
+
+                case "6":
+                    // определяем и запускаем задачи
+                    var task0001 = SquareAsync(4);
+                    var task0002 = SquareAsync(5);
+                    var task0003 = SquareAsync(6);
+
+                    await Task.WhenAll(task0001, task0002, task0003);
+                    // получаем результат задачи task2
+                    Console.WriteLine($"task2 result: {task0002.Result}"); // task2 result: 25
+                    Console.ReadKey();
+                    return true;
+                case "0":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        // --- 01 ---
+        async static Task PrintAsync(string message)
+        {
+            await Task.Delay(2000);     // имитация продолжительной операции
+            Console.WriteLine(message);
+        }
+        // --- 02 ---
+        // --- 03 ---
+        // --- 04 ---
+        async static Task PrintAsync_04(string message)
+        {
+            await Task.Delay(new Random().Next(1000, 2000));     // имитация продолжительной операции
+            Console.WriteLine(message);
+        }
+        // --- 05 ---
+        async static Task<int> SquareAsync(int n)
+        {
+            await Task.Delay(1000);
+            return n * n;
+        }
+        // --- 06 ---
+    }
+}
